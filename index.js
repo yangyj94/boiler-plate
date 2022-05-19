6c1997e5b5871f00844123490a6b7c239cd1fe93
lani.yang <lani.yang@kakaocorp.com>
2022-05-19 09:58:13 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+
+const config = require('./config/key')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yujinyang:diddb123@education.zrs1p.mongodb.net/?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!ㅎㅇㅎㅇㅎㅇ')
+  res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  //회원가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+  
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({ success: true})
+  })
 })
 
 app.listen(port, () => {
